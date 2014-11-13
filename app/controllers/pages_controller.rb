2b70031a6035c40d9d64d99ba127868f19057592
Zeeshan Malik <zeeshan.m.malik@gmail.com>
2014-11-13 02:31:36 -0500

    more info added ..

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,31 +1,41 @@
 class PagesController < ApplicationController
   def index
+    @active = 'home'
   end
 
   def interact_basic
+    @active = 'interact_basic'
   end
 
   def interact_advanced
+    @active = 'interact_advanced'
   end
 
   def references
+    @active = 'references'
   end
 
   def discussion
+    @active = 'discussion'
   end
 
   def occ_model
+    @active = 'occ_model'
   end
 
   def sw_vignette
+    @active = 'sw_vignette'
   end
 
   def sw_act_sub_verb_obj
+    @active = 'sw_act_sub_verb_obj'
   end
 
   def interact_advanced_report
+    @active = 'interact_advanced'
   end
 
   def interact_basic_report
+    @active = 'interact_basic'
   end
 end
